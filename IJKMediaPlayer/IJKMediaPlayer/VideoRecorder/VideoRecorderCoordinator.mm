d19801d8fb103512efe94af1fed2ab07c4d49484
AndrewXF <410337505@qq.com>
2017-03-16 23:03:00 +0800

    IJKMediaPlayer

---
+++ IJKMediaPlayer/IJKMediaPlayer/VideoRecorder/VideoRecorderCoordinator.mm
@@ -0,0 +1,63 @@
+//
+//  VideoRecorderCoordinator.m
+//  IJKMediaPlayer
+//
+//  Created by ning on 16/4/30.
+//  Copyright © 2016年 bilibili. All rights reserved.
+//
+
+#import "VideoRecorderCoordinator.h"
+#import "VideoRecorder.hpp"
+
+
+@implementation VideoRecorderCoordinator
+
+
+- (void)setDelegate:(id<VideoRecorderCoordinatorDelegate>)delegate callbackQueue:(dispatch_queue_t)delegateCallbackQueue
+{
+    if(delegate && ( delegateCallbackQueue == NULL)){
+        @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Caller must provide a delegateCallbackQueue" userInfo:nil];
+    }
+    @synchronized(self)
+    {
+        _delegate = delegate;
+        if (delegateCallbackQueue != _delegateCallbackQueue){
+            _delegateCallbackQueue = delegateCallbackQueue;
+        }
+    }
+}
+
+- (void)coorInitRecorder:(NSString *)fileName srcHight:(int)srcHight cameraSelection:(int)cameraSelection audioBitrate:(long)audioBitrate videoBitrate:(long)videoBitrate hasAudio:(int)hasAudio
+{
+    initRecorder((char*)[fileName UTF8String], srcHight, cameraSelection, audioBitrate, videoBitrate, hasAudio);
+    
+    if ([self.delegate respondsToSelector:@selector(recorder:didFinishInit:)]) {
+        [self.delegate recorder:self didFinishInit:nil];
+    }
+    
+}
+
+- (void)coorSetRecorderInfo:(int)srcHight cameraSelection:(int)cameraSelection
+{
+    setRecorderInfo(srcHight, cameraSelection);
+}
+
+- (void)coorRecordeVideo:(void *)frame timestamp:(long)timestamp
+{
+    recordeVideo(frame, timestamp);
+}
+
+- (void)coorRecordeAudio:(void *)samples numSamples:(long)numSamples
+{
+    recordeAudio(samples, numSamples);
+}
+
+- (void)coorRecordeClose
+{
+    recordeClose();
+    if ([self.delegate respondsToSelector:@selector(recorder:didFinishRecord:)]) {
+        [self.delegate recorder:self didFinishRecord:nil];
+    }
+}
+
+@end
